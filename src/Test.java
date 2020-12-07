4ce0c967b9090dd92492e19db14773508ae3d3e5
ex-weic001 <ex-weic001@cmft.com>
2020-12-07 18:35:48 +0800

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,20 @@
+public class Test {
+    public static void main(String[] args) {
+        /*
+         * 可以的
+         */
+        System.out.println("bbasdfhjjjjjjjjjjjjjjjhjkallllssssssssssssssssss");
+        System.out.println("bb");
+        System.out.println("bb");
+        //hah
+        /*
+        haooaoh
+         */
+        System.out.println("bb");
+        System.out.println("bb");
+        System.out.println("bb");
+        System.out.println("bb");
+        System.out.println("bb");
+
+    }
+}
